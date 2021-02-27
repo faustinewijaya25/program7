749a9c97f224075aac762e46ac5275b332848302
Faustine wijaya <32315692+evelynwijaya@users.noreply.github.com>
2021-02-27 21:14:23 +0800

    program7

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pweb201/srv1/html/program7/';
+$config['base_url'] = getenv('BASE_URL_PROGRAM7');
 
 /*
 |--------------------------------------------------------------------------
